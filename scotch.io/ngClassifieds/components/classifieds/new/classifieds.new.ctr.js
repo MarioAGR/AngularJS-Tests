6588f8cabcce79e016ea05097326c1436d2dd53d
Mario Guillen <marioagr95@outlook.com>
2017-04-19 19:13:12 -0600

    Finished Routing with UI-Router

---
+++ scotch.io/ngClassifieds/components/classifieds/new/classifieds.new.ctr.js
@@ -0,0 +1,45 @@
+(function() {
+
+    "use strict";
+
+    angular
+        .module('ngClassifieds')
+        .controller('newClassifiedsCtrl', function($scope, $state, $mdSidenav, $timeout, $mdDialog, classifiedsFactory) {
+
+            var vm = this;
+
+            vm.closeSidebar = closeSidebar;
+            vm.saveClassified = saveClassified;
+
+            $timeout(function() {
+                $mdSidenav('left').open();
+            });
+
+            $scope.$watch('vm.sidenavOpen', function(sidenav) {
+                if (sidenav === false) {
+                    $mdSidenav('left')
+                        .close()
+                        .then(function() {
+                            $state.go('classifieds');
+                        });
+                }
+            });
+
+            function closeSidebar() {
+                vm.sidenavOpen = false;
+            }
+
+            function saveClassified(classified) {
+                if (classified) {
+                    classified.contact = {
+                        name: "Mario",
+                        phone: "(555) 555-5555",
+                        email: "dasdasda@mail.com"
+                    }
+                    $scope.$emit('newClassified', classified);
+                    vm.sidenavOpen = false;
+                }
+            }
+
+        })
+})();
